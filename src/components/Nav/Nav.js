74f87b89185b9d3129309410ec08c8d73f101607
earuiz1 <earuiz1196@gmail.com>
2022-10-03 20:07:58 -0500

    Installed tailwindcss and finished working on the first mock-up of  the nav component.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+
+const Nav = () => {
+  return (
+    <React.Fragment>
+      <nav className="flex justify-between align-center bg-gray-800 py-6 px-4">
+        <div className="flex gap-3">
+          <TiWeatherPartlySunny className="text-white" size={28} />
+          <h1 className="text-xl text-slate-50 font-bold">Weather App</h1>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Nav;
